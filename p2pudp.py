428b8f895d684c39c5f2294f3a622dc4ae9f310d
Vladimir Kirovskiy <vkirovskiy@gmail.com>
2015-09-14 03:15:30 +0600

    Added second worker to open many udp holes (for external command)

--- p2pudp.py
+++ p2pudp.py
@@ -6,18 +6,21 @@
 # server response -> (client-1 is 192.168.1.1:1234) -> client-2
 
 import select
+import socket
 import sys
 from time import time
 import argparse
 from p2pserver import pServerWorker
 import signal
-
+from p2pusercmd import user_console
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--myid", help="id to find you")
 parser.add_argument("-p", type=int, help="server port")
 parser.add_argument("server", help="helper server ip")
 parser.add_argument("-l", help="log file with packet trace")
+parser.add_argument("--connect", help="auto connect to")
+parser.add_argument("--wait", help="wait client and exit")
 
 args = parser.parse_args()
 if args.myid:
@@ -30,28 +33,51 @@ if args.server:
     else:
         SRVPORT=8001
 
-pworker = pServerWorker(SERVER, SRVPORT, myid)
-if args.l: pworker.packetlog = args.l
+pworker = pServerWorker(SERVER, SRVPORT, myid, args.connect)
+pworker_slave = pServerWorker(SERVER, SRVPORT, myid + "-slave", '')
 
-signal.signal(signal.SIGINT, pworker.sig_exit_handler)
+def sig_exit(sig, f):
+    pworker.sig_exit_handler()
+    pworker_slave.sig_exit_handler()
+    sys.exit(0)
+
+if args.l: pworker.packetlog = args.l
+if args.wait: pworker_slave.wait_client = args.wait
+
+signal.signal(signal.SIGINT, sig_exit)
 
 pworker.register()
+pworker_slave.register()
 t1 = time()
 
+print type(pworker.socket)
+print type(sys.stdin)
+
+socks = [pworker.socket, pworker_slave.socket, sys.stdin]
+
 while True:
-    r, w, e = select.select([pworker.socket, sys.stdin],[],[],10)
+    r, w, e = select.select(socks,[],[],10)
+
     for s in r:
         if s == pworker.socket:
             pworker.recv_data()
+        elif s == pworker_slave.socket:
+            pworker_slave.recv_data()
         elif s == sys.stdin:
             line = s.readline()
-            pworker.user_console(line) 
+            user_console(pworker, line) 
         else:
             print "Unknown socket"
 
+    if not pworker_slave:
+        print "Slave died. Restarting"
+        pworker_slave = pServerWorker(SERVER, SRVPORT, myid + "-slave", '')
+
     t2 = time()
-    if t2-t1>10: 
+    if t2-t1>5: 
         pworker.register()
+        pworker_slave.register()
         pworker.send_ka_to_clients()
+        #pworker_slave.send_ka_to_clients()
         t1=t2
 
